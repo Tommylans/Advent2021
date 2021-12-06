2e15399a37854091e2905a192cee5a6f5102cae9
Tom <tom@devv.nl>
2021-12-06 10:40:01 +0100

    Made a function for simulation

--- day6/generic.go
+++ day6/generic.go
@@ -19,3 +19,27 @@ func parseInput(scanner *bufio.Scanner) (out []int) {
 
 	return
 }
+
+func simulateFishGroup(input []int, days int) int {
+	fishes := [9]int{}
+
+	for _, in := range input {
+		fishes[in]++
+	}
+
+	for i := 0; i < days; i++ {
+		toBeCreated := fishes[0]
+		for j := 1; j < len(fishes); j++ {
+			fishes[j-1] = fishes[j]
+		}
+
+		fishes[6] += toBeCreated
+		fishes[8] = toBeCreated
+	}
+
+	out := 0
+	for _, fishGroup := range fishes {
+		out += fishGroup
+	}
+	return out
+}
--- day6/part2.go
+++ day6/part2.go
@@ -20,26 +20,7 @@ func RunPart2(scanner *bufio.Scanner) (int, time.Duration, time.Duration) {
 	inputDuration := time.Since(inputStart)
 
 	logicStart := time.Now()
-	fishes := [9]int{}
-
-	for _, in := range input {
-		fishes[in]++
-	}
-
-	for i := 0; i < 256; i++ {
-		toBeCreated := fishes[0]
-		for j := 1; j < len(fishes); j++ {
-			fishes[j-1] = fishes[j]
-		}
-
-		fishes[6] += toBeCreated
-		fishes[8] = toBeCreated
-	}
-
-	out := 0
-	for _, fishGroup := range fishes {
-		out += fishGroup
-	}
+	out := simulateFishGroup(input, 256)
 	logicDuration := time.Since(logicStart)
 
 	return out, inputDuration, logicDuration
